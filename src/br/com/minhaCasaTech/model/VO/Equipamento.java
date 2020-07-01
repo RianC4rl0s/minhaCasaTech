3f87ea3200416d9ce082f363c03eec31506da050
RianC4rl0s <riancarlos.sl@gmail.com>
2020-07-01 10:45:40 -0300

    get/set Equipamento.java

--- src/br/com/minhaCasaTech/model/VO/Equipamento.java
+++ src/br/com/minhaCasaTech/model/VO/Equipamento.java
@@ -43,6 +43,41 @@ public class Equipamento {
 	
 	}
 	public double getPreco() {
+		
 		return preco;
 	}
-
+	public void setQuantidade(int quantidade) {
+		if (quantidade <= 0) {
+			System.out.println("Valor invalido");
+		}else {
+			this.quantidade = quantidade;
+		}
+	}
+	public int getQuantidade() {
+	 return quantidade;
+	}
+	public void setNumeroSDeSerie(int numeroDeSerie) {
+		if (quantidade <= 0) {
+			System.out.println("Valor invalido");
+		}else {
+			this.numeroDeSerie = numeroDeSerie;
+		}
+	}
+	public int getNumeroDeSerie() {
+	 return numeroDeSerie;
+	}
+	public void setLocal(String local) {
+		if(local != null) {
+			if(local.equals("")) {
+				System.out.println("Vazio");
+			}else {
+				this.local = local;
+			}
+		}else {
+			System.out.println("O valor esta nulo");
+		}
+	}
+	public String getLocal() {
+		return local;
+	}
+}
