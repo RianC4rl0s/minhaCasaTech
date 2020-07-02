6fda6cc4e7d7f5a9d4a4cedfc17ff67cc2d4fd0a
RianC4rl0s <riancarlos.sl@gmail.com>
2020-07-02 11:07:16 -0300

    Local,Responsavel,Relatorio, Get Set

--- src/br/com/minhaCasaTech/model/VO/Local.java
+++ src/br/com/minhaCasaTech/model/VO/Local.java
@@ -1,5 +1,30 @@
 package br.com.minhaCasaTech.model.VO;
 
 public class Local {
-
+	private String casa;
+	private String compartimento;
+	//Set
+	public void setCasa(String casa) {
+		if(casa ==null) {
+			System.out.println("ValorNulo");
+			
+		}else {
+			this.casa = casa;
+		}
+	}
+	public void setCompartimento(String compartimento) {
+		if(compartimento==null) {
+			System.out.println("ValorNulo");
+			
+		}else {
+			this.compartimento= compartimento;
+		}
+	}
+	//Get
+	public String getCasa() {
+		return casa;
+	}
+	public String getCompartimento() {
+		return compartimento;
+	}
 }
--- src/br/com/minhaCasaTech/model/VO/Responsavel.java
+++ src/br/com/minhaCasaTech/model/VO/Responsavel.java
@@ -1,5 +1,45 @@
 package br.com.minhaCasaTech.model.VO;
 
 public class Responsavel {
-
+	private String nome;
+	private String endereco;
+	private String telefone;
+	
+	//Set
+	public void setNome(String nome) {
+		if(nome == null) {
+			System.out.println("valor nulo");
+			
+		}else {
+			this.nome = nome;
+		}
+	}
+	public void setEndereco(String endereco) {
+		if(endereco== null) {
+			System.out.println("valor nulo");
+			
+		}else {
+			this.endereco= endereco;
+		}
+	}
+	public void setTelefone(String telefone) {
+		if(telefone== null) {
+			System.out.println("valor nulo");
+			
+		}else {
+			this.telefone= telefone;
+		}
+	}
+	
+	//Get
+	public String getNome() {
+		return nome;
+	}
+	public String getEndereco(){
+		return endereco;
+		
+	}
+	public String getTelefone() {
+		return telefone;
+	}
 }
