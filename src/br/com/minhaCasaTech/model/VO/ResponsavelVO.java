bcfbd901be83626a0f96de9ecdc755ca058e77ca
Junior Cunha <manoelricardo38@gmail.com>
2020-07-09 17:09:36 -0300

    Organizing ResponsavelVO

--- src/br/com/minhaCasaTech/model/VO/ResponsavelVO.java
+++ src/br/com/minhaCasaTech/model/VO/ResponsavelVO.java
@@ -1,60 +1,51 @@
 package br.com.minhaCasaTech.model.VO;
 
 public class ResponsavelVO extends PessoaVO{
+	
 	private String login;
 	private String senha;
-
 	
-	//Set
-	public void setLogin(String login) {
-		
-		//PRECISA FAZER UM METODO PARA CHAMAR O DAO, E VERIFICAR SE O USUARIO JA EXISTE;
-		if(login ==null) {
-			System.out.println("ValorNulo");
-			
-		}else {
-			if(login.equals("")) {
-				System.out.println("Nada escrito");
-			}else {
-				System.out.println(this.login);
-			this.login = login;
-			}
-		
-		}
-	}
-	public void setSenha(String senha) {
-		if(senha == null) {
-			System.out.println("Valor nulo");
-		}else {
-			if(senha.equals("")) {
-				System.out.println("Nada foi escrito");
-			}else {
-				if(senha.matches("[a-zA-Z][0-9]")  == true) {
-					this.senha =  senha;
-					System.out.println(this.senha);
-				}else if(senha.matches("[a-zA-Z][0-9]")== false) {
-					System.out.println("A senha deve conter caracteres Maiusculos, minusculos e numeros");
-				}
-			}
-			
-		}
-		
-	}
-	
-	//Get
-	public String getLogin() {
-		return login;
-	}
-	public String getSenha() {
-		return senha;
-		
-	}
-	public ResponsavelVO() {
-		super();
-	}
 	public ResponsavelVO(String nome,String endereco,String login,String senha) {
 		super(nome, endereco);
 		setLogin(login);
 		setSenha(senha);
+	}
+
+	//Set
+	
+	public void setLogin(String login) {
+		
+		//PRECISA FAZER UM METODO PARA CHAMAR O DAO, E VERIFICAR SE O USUARIO JÁ EXISTE;
+		if(login ==null)
+			System.out.println("ValorNulo");
+		else
+			if(login.equals(""))
+				System.out.println("Nada escrito");
+			else
+				this.login = login;
+	}
+	
+	public void setSenha(String senha) {
+		if(senha == null)
+			System.out.println("Valor nulo");
+		else
+			if(senha.equals(""))
+				System.out.println("Nada foi escrito");
+			else
+				if(senha.matches("(.*)([a-zA-Z][0-9])(.*)") == true)
+					this.senha =  senha;
+				else
+					System.out.println("A senha deve conter caracteres Maiusculos, minusculos e numeros");
+	}
+	
+	//Get
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public String getSenha() {
+		return senha;
+		
 	}
 }
