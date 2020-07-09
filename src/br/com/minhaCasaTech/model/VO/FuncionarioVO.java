9fd26c5fba9693e562e619dc3efced2714d25356
RianC4rl0s <riancarlos.sl@gmail.com>
2020-07-09 10:45:33 -0300

    FuncionarioVO ProprietarioVO

---
+++ src/br/com/minhaCasaTech/model/VO/FuncionarioVO.java
@@ -0,0 +1,9 @@
+package br.com.minhaCasaTech.model.VO;
+
+public class FuncionarioVO extends ResponsavelVO{
+
+	
+	public FuncionarioVO(String nome,String endereco,String login,String senha) {
+		super(nome,endereco,login,senha);
+	}
+}
---
+++ src/br/com/minhaCasaTech/model/VO/ProprietarioVO.java
@@ -0,0 +1,12 @@
+package br.com.minhaCasaTech.model.VO;
+
+public class ProprietarioVO extends ResponsavelVO {
+
+	
+	
+	
+	public ProprietarioVO(String nome,String endereco,String login,String senha) {
+		super(nome,endereco,login,senha);
+	}
+	
+}
