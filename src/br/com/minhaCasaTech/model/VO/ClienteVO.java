dff6f94ebedad5e853e15edd88d0445d2ebeeb16
Junior Cunha <manoelricardo38@gmail.com>
2020-07-09 17:12:07 -0300

    Organizing ClientVO

--- src/br/com/minhaCasaTech/model/VO/ClienteVO.java
+++ src/br/com/minhaCasaTech/model/VO/ClienteVO.java
@@ -1,38 +1,33 @@
 package br.com.minhaCasaTech.model.VO;
 
 public class ClienteVO extends PessoaVO {
+	
 	private String cpf;
 	
-	public void setCpf(String cpf) {
-		if(cpf == null) {
-			System.out.println("Valor nulo");
-			
-		}else{
-			if(cpf.equals("")) {
-				System.out.println("SEM CPF");
-			}else {
-				if(cpf.length() == 11) {
-					this.cpf = cpf;
-				}else {
-					System.out.println("Formato nao padrao");
-				}
-			}
-		}
-	}
-	
-	public String getCpf(){
-		
-	return cpf;
-	}
-	public String toString() {
-		String s = getCpf();
-		return s;
-	}
-	public ClienteVO() {
-		super();
-	}
 	public ClienteVO(String nome,String endereco, String cpf) {
 		super(nome, endereco);
 		setCpf(cpf);
+	}
+	
+	public void setCpf(String cpf) {
+		if(cpf == null)
+			System.out.println("Valor nulo");
+		else
+			if(cpf.equals(""))
+				System.out.println("SEM CPF");
+			else
+				if(cpf.length() == 11)
+					this.cpf = cpf;
+				else
+					System.out.println("CPF inválido");
+	}
+	
+	public String getCpf(){
+		return cpf;
+	}
+	
+	public String toString() {
+		String s = getCpf();
+		return s;
 	}
 }
