6c58d3c2d8fb45ba2269ed763c821b23ad8a01bf
Junior Cunha <manoelricardo38@gmail.com>
2020-07-09 17:14:17 -0300

    Organizing ResposavelVO' sons

--- src/br/com/minhaCasaTech/model/VO/FuncionarioVO.java
+++ src/br/com/minhaCasaTech/model/VO/FuncionarioVO.java
@@ -2,8 +2,8 @@ package br.com.minhaCasaTech.model.VO;
 
 public class FuncionarioVO extends ResponsavelVO{
 
-	
 	public FuncionarioVO(String nome,String endereco,String login,String senha) {
 		super(nome,endereco,login,senha);
 	}
+	
 }
