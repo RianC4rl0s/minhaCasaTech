2139787eb2c09daf5ec9e6239f96c1d7416b1dbc
RianC4rl0s <riancarlos.sl@gmail.com>
2020-07-19 13:42:19 -0300

    FrontController, Telas

--- src/br/com/minhaCasaTech/controller/FrontController.java
+++ src/br/com/minhaCasaTech/controller/FrontController.java
@@ -1,7 +1,92 @@
 package br.com.minhaCasaTech.controller;
 
+import br.com.minhaCasaTech.view.Telas;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class FrontController{
+	
+	@FXML Button cancelar_btm = new Button();
+	
+	@FXML
+	private void cancelarOperacao(){
+	    Stage stage = (Stage) cancelar_btm.getScene().getWindow(); //Obtendo a janela atual
+	    stage.close(); //Fechando o Stage
+	}
+	public void sair() {
+		try {
+			Telas.telaLogin();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//Tela login
+		@FXML TextField login_txf;
+		@FXML TextField senha_pf;
+		@FXML Label erro_login_lb;
+		@FXML Label error_senha_lb;
 		
-
+		public void autenticar(){
+			try {
+				Telas.telaPrincipal();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	//Telap principal	
+	public void gerenciarEquipamento() {
+		try {
+			Telas.telaGerenciarEquipamento();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void cadastrarEquipamento() {
+		try {
+			Telas.telaCadastrarEquipamento();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void gerenciarResponsavel() {
+		try {
+			Telas.telaGerenciarResponsavel();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void cadastrarResponsavel() {
+		try {
+			Telas.telaCadastrarResponsavel();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void gerenciarLocal() {
+		try {
+			Telas.telaCadastrarEquipamento();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void gerarRelatorio() {
+		try {
+			Telas.telaCadastrarEquipamento();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
---
+++ src/br/com/minhaCasaTech/view/Telas.java
@@ -0,0 +1,85 @@
+package br.com.minhaCasaTech.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Telas extends Application {
+private static Stage primaryStage;
+	
+	
+	public static Stage getPrimaryStage() {
+		return primaryStage;
+	}
+	public static void setPrimaryStage(Stage primaryStage) {
+		Telas.primaryStage = primaryStage;
+	}
+	
+	public static void main(String args[]) {
+		launch();
+		}
+		
+	public void start(Stage primaryStage) throws Exception {
+		setPrimaryStage(primaryStage);
+		primaryStage.setTitle("MinhaCasaTECH");
+		primaryStage.show();
+		telaLogin();
+		
+		
+	}
+	
+	public static void telaLogin() throws Exception{
+		Parent root = FXMLLoader.load(Telas.class.getResource("VE/TelaLogin.fxml"));
+		
+		Scene cena = new Scene(root);
+		
+		
+		primaryStage.setScene(cena);
+	}
+	public static void telaPrincipal() throws Exception{
+		Parent root = FXMLLoader.load(Telas.class.getResource("VE/TelaPrincipal.fxml"));
+		
+		Scene cena = new Scene(root);
+		
+		
+		primaryStage.setScene(cena);
+		
+	}
+	public static void telaCadastrarEquipamento() throws Exception{
+		Parent root = FXMLLoader.load(Telas.class.getResource("VE/TelaCadastrarEquipamento.fxml"));
+		
+		Scene cena = new Scene(root);
+		Stage secondarySage = new Stage();
+		secondarySage.setTitle("MinhaCasaTECH");
+		secondarySage.show();
+		secondarySage.setScene(cena);
+		
+		
+	}
+	public static void telaGerenciarEquipamento() throws Exception{
+		Parent root = FXMLLoader.load(Telas.class.getResource("VE/TelaGerenciarEquipament.fxml"));
+		
+		Scene cena = new Scene(root);
+		
+		
+		primaryStage.setScene(cena);
+		
+	}
+	public static void telaCadastrarResponsavel() throws Exception{
+		Parent root = FXMLLoader.load(Telas.class.getResource("VE/TelaCadastrarResponsavel.fxml"));
+		Scene cena = new Scene(root);
+		Stage secondarySage = new Stage();
+		secondarySage.setScene(cena);
+		secondarySage.setTitle("MinhaCasaTECH");
+		secondarySage.show();
+	
+	}
+	public static void telaGerenciarResponsavel() throws Exception{
+		Parent root = FXMLLoader.load(Telas.class.getResource("VE/TelaGerenciarResponsavel.fxml"));
+		Scene cena  = new Scene(root);
+		
+		primaryStage.setScene(cena);
+	}
+}
